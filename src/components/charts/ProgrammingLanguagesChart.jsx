19285586e9c1bd82e6ea3e66d8ff9215574ff22e
LoulouPlou <leia.plourde@hotmail.com>
2025-12-14 19:42:28 -0500

    test api

--- src/components/charts/ProgrammingLanguagesChart.jsx
+++ src/components/charts/ProgrammingLanguagesChart.jsx
@@ -2,55 +2,67 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { createTopLanguagesChartStyles } from "../../styles/components/charts/firstChart";
 import { useTheme } from "../../styles/theme";
-
+ 
 export default function ProgrammingLanguagesChart({ data, title, metadata }) {
     const theme = useTheme();
     const styles = createTopLanguagesChartStyles(theme);
-    const maxValue = 1000;
-    data = [
-        { label: "Python", value: 913 },
-        { label: "Java", value: 803 },
-        { label: "JavaScript", value: 522 },
-        { label: "C", value: 352 },
-        { label: "TypeScript", value: 293 },
-        { label: "PHP", value: 234 },
-        { label: "Go", value: 164 },
-        { label: "PowerShell", value: 129 },
-        { label: "Shell", value: 124 },
-        { label: "Kotlin", value: 114 },
-    ];
-
+    const maxValue = 1500;
+    // data = [
+    //     { label: "Python", value: 913 },
+    //     { label: "Java", value: 803 },
+    //     { label: "JavaScript", value: 522 },
+    //     { label: "C", value: 352 },
+    //     { label: "TypeScript", value: 293 },
+    //     { label: "PHP", value: 234 },
+    //     { label: "Go", value: 164 },
+    //     { label: "PowerShell", value: 129 },
+    //     { label: "Shell", value: 124 },
+    //     { label: "Kotlin", value: 114 },
+    // ];
+    const COLORS = {
+        light1: "rgba(210, 240, 241, 1)",
+        light2: "#9ad1d4",
+        mid: "#62B6CB",
+        mid2: "#2C7DA0",
+        dark: "#1b4865",
+    };
+ 
     return (
         <View>
             <Text style={styles.title}>Top 10 Programming Languages</Text>
-
+ 
             {data.map((item, index) => (
                 <View key={index} style={styles.row}>
-
-
                     <Text style={styles.label}>{item.label}</Text>
-
-
+ 
                     <View style={styles.barContainer}>
                         <View
                             style={[
                                 styles.bar,
                                 {
-                                    width: `${(item.value / maxValue) * 100}%`,
-                                    backgroundColor: theme.colors.accent,
+                                    width: `${Math.min(item.value, maxValue) / maxValue * 100}%`,
+                                    backgroundColor:
+                                        index === 0 ? COLORS.dark :
+                                            index === 1 ? COLORS.mid2 :
+                                                index === 2 ? COLORS.mid :
+                                                    index >= 3 && index <= 5 ? COLORS.light2 :
+                                                        COLORS.light1,
+ 
                                 },
                             ]}
                         />
+ 
                     </View>
-
-                    <Text style={[styles.value, { color: theme.colors.textSecondary }]}>
-                        {item.value}
-                    </Text>
-
+ 
+                    <Text style={styles.value}>{item.value}</Text>
+ 
+ 
                 </View>
-
+ 
             ))}
-
+            <Text style={styles.footerLabel}>
+                Number of mentions
+            </Text>
         </View>
     );
 }
--- src/styles/components/charts/firstChart.js
+++ src/styles/components/charts/firstChart.js
@@ -1,49 +1,54 @@
 import { StyleSheet } from "react-native";
-
+ 
 export const createTopLanguagesChartStyles = (theme) =>
   StyleSheet.create({
     container: {
       marginBottom: theme.spacing.lg,
     },
+ 
     title: {
+      marginTop: 20,
       marginBottom: theme.spacing.md,
       ...theme.typography.h4,
       color: theme.colors.textPrimary,
       textAlign: "center",
+ 
     },
-
+ 
     row: {
       flexDirection: "row",
       alignItems: "center",
-      marginBottom: theme.spacing.sm ?? 10,
+      marginBottom: 16,
     },
-
     label: {
-      width: 90,
-      fontSize: 13,
-      color: theme.colors.textPrimary,
-    },
-
-    barContainer: {
-      flex: 1,
-      height: 10,
-      backgroundColor: theme.colors.tag ?? "#EAEAEA",
-      borderRadius: theme.borderRadius.sm ?? 6,
-      marginHorizontal: theme.spacing.sm ?? 8,
-      overflow: "hidden",
-    },
-
-    bar: {
-      height: "100%",
-      borderRadius: theme.borderRadius.sm ?? 6,
-      backgroundColor: theme.colors.accent,
-    },
-
-    value: {
-      width: 40,
+      width: 75,
       fontSize: 12,
       textAlign: "right",
+      paddingRight: 6,
+      color: theme.colors.textPrimary,
+    },
+ 
+ 
+    barContainer: {
+      flex: 1,
+      height: 28,
+      marginRight: 6,
+    },
+ 
+    bar: {
+      height: 28,
+      backgroundColor: theme.colors.accent,
+    },
+ 
+    value: {
+      fontSize: 12,
       color: theme.colors.textSecondary,
     },
-
+    footerLabel: {
+      marginTop: 8,
+      textAlign: "center",
+      fontSize: 12,
+      color: theme.colors.textSecondary,
+    },
+ 
   });
